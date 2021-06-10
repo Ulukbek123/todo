9f9afc384e669b5e778d820c226423ddc1e855d4
Ulukbek <aidaraliev123@gmail.com>
2021-06-10 18:41:19 +0600

    added mark and unmark def and delete

--- main/views.py
+++ main/views.py
@@ -5,21 +5,48 @@ from .models import ToMeet
 # Create your views here.
 def homepage(request):
     return render(request, "index.html")
+
+
 def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
+
+
 def meeting(request):
     tomeet_list = ToMeet.objects.all()
     return render(request, "meeting.html", {"tomeet_list": tomeet_list})
+
+
 def add_todo(request):
     form = request.POST
     text = form["todo_text"]
     todo = ToDo(text=text)
     todo.save()
     return redirect(test)
+
+
 def add_tomeet(request):
     form = request.POST
     text = form["tomeet_text"]
     tomeet = ToMeet(name=text)
     tomeet.save()
     return redirect(meeting)
+
+
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
+def delete_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect(test)
+
+
+def unmark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = False
+    todo.save()
+    return redirect(test)
